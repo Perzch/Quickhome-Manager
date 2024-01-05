2699a417c712c559cecf5e15c85757cb675b8e77
Perzch <perzch@qq.com>
2024-01-05 14:22:37 +0800

    富文本深色主题适应

--- src/components/RichInput.vue
+++ src/components/RichInput.vue
@@ -1,4 +1,15 @@
 <script setup>
+//Import Froala Editor
+import 'froala-editor/js/plugins.pkgd.min.js';
+//Import third party plugins
+import 'froala-editor/js/third_party/embedly.min';
+import 'froala-editor/js/third_party/font_awesome.min';
+import 'froala-editor/js/third_party/spell_checker.min';
+import 'froala-editor/js/third_party/image_tui.min';
+import 'froala-editor/js/languages/zh_cn.js'
+// Import Froala Editor css files.
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/css/froala_style.min.css';
 const config = {
   placeholderText: '请在此输入',
   language: 'zh_cn', //语言
--- src/main.js
+++ src/main.js
@@ -5,17 +5,6 @@ import { createPinia } from 'pinia'
 import { MotionPlugin } from '@vueuse/motion'
 // froala editor
 import VueFroala from 'vue-froala-wysiwyg';
-//Import Froala Editor
-import 'froala-editor/js/plugins.pkgd.min.js';
-//Import third party plugins
-import 'froala-editor/js/third_party/embedly.min';
-import 'froala-editor/js/third_party/font_awesome.min';
-import 'froala-editor/js/third_party/spell_checker.min';
-import 'froala-editor/js/third_party/image_tui.min';
-import 'froala-editor/js/languages/zh_cn.js'
-// Import Froala Editor css files.
-import 'froala-editor/css/froala_editor.pkgd.min.css';
-import 'froala-editor/css/froala_style.min.css';
 
 
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
